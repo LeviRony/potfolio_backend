231c4aedc7eccdc0b9e35eaf948ca765d69fee61
RonyBoom <ronylevi99@gmail.com>
2025-03-29 18:37:34 +0300

    skill_rating type == number

--- models/skillsModel.js
+++ models/skillsModel.js
@@ -2,7 +2,10 @@ const postgres = require('../config/postgres');
 
 async function getAllSkillsSQL() {
     const result = await postgres.query('SELECT skill_id AS id, skill_name, skill_rating FROM skills;');
-    return result.rows;
+    return result.rows.map(row => ({
+        ...row,
+        skill_rating: +row.skill_rating
+    }))
 }
 
 async function getSkillByIdSQL(id) {
