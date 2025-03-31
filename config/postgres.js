0325cbce0a3e5bf23fa2ab4184cb0f8f78dc2374
RonyBoom <ronylevi99@gmail.com>
2025-03-31 21:04:19 +0300

    README.MD

--- config/postgres.js
+++ config/postgres.js
@@ -4,7 +4,7 @@ const pool = new Pool({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     database: process.env.DB_DATABASE,
-    password: '123',
+    password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
 })
 
